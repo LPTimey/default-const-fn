9ee3039db9e2fb6c27d84d7bd482df0347209578
LPTimey <86461861+LPTimey@users.noreply.github.com>
2024-02-07 00:59:08 +0100

    lib.rs aktualisieren

--- src/lib.rs
+++ src/lib.rs
@@ -190,13 +190,19 @@ pub fn no_op(
 #[proc_macro]
 pub fn set_lints(input: proc_macro::TokenStream)
 -> proc_macro::TokenStream{
+    let lint = lints();
+    quote::quote!{
+     #lint
+
+    #input}.into()
+}
+
+fn lints()->proc_macro::Tokenstream{
     quote::quote!{
      #![warn(
      clippy::enum_glob_use,
      clippy::pedantic,
      clippy::nursery,
      clippy::unwrap_used,
- )]
-
-    input}.into()
+ )]}.into()
 }
